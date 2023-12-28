44d4c9cae09f4c6a24dde70d1d065cf96bfeb8f2
Zehan Khan <zkhan@hootboard.com>
2023-12-28 16:12:08 +0530

    feat: :sparkles: create video model

--- shell.sh
+++ shell.sh
@@ -24,3 +24,9 @@ python3 manage.py runserver
 
 # create app for videos
 python3 manage.py startapp videos
+
+# make migration after createing models
+python3 manage.py makemigrations
+
+# migrate
+python3 manage.py migrate     
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    slug = models.SlugField(blank=True,null=True)
+    video_id = models.CharField()
