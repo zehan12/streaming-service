5b9a2ce88bfa0c01d6dd18af7e3fb3d36d8a3259
Zehan Khan <zkhan@hootboard.com>
2023-12-29 01:19:16 +0530

    feat: :sparkles: add proxy Video model

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Video
+from .models import Video, VideoProxy
 
 admin.site.register(Video)
+
+# add proxy model admin
+admin.site.register(VideoProxy)
+ 
--- src/videos/models.py
+++ src/videos/models.py
@@ -6,3 +6,9 @@ class Video(models.Model):
     description = models.TextField(blank=True,null=True)
     slug = models.SlugField(blank=True,null=True)
     video_id = models.CharField(max_length=220)
+
+class VideoProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
